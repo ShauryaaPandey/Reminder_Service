763c5b9c628751f7699be7520b4484f1619e6266
Shaurya Pandey <shauryapandey40@gmail.com>
2026-04-05 15:59:25 +0530

    Setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const {PORT} = require('./config/server-config');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -11,7 +13,16 @@ const setupAndStartServer = () => {
 
     app.listen(PORT,() => {
         console.log(`server started at PORT : ${PORT}`);
+
+        sendBasicEmail(
+        'support@admin.com', //from
+        '24363@iiitu.ac.in', //to
+        'This is a testing Email', //subj
+        'Hey! , I hope u like the support' //text
+        );
     });
+
+    
 }
 
 setupAndStartServer();
